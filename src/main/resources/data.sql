1d802c51bdf54084250c2594d5d75b82bffaaea8
Ryan Beckman <ryan.beckman@accenture.com>
2020-12-11 19:11:12 +0900

    JPA Base RestServices Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'ryan@fake.com', 'ryan', 'beckman', 'master', '12345678', 'ryan');
+insert into user values(102, 'ryan2@fake.com', 'bryan', 'bbeckman', 'master', '12345679', 'bryan');
+insert into user values(103, 'ryan3@fake.com', 'tryan', 'tbeckman', 'master', '12345680', 'tryan');
