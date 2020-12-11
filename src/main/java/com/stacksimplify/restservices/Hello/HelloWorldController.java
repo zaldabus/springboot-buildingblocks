e9134b4f28db9329d06d8abd712b9f62daeb6da2
Ryan Beckman <ryan.beckman@accenture.com>
2020-12-11 15:32:33 +0900

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller
+@RestController
+public class HelloWorldController {
+
+  // Simple Method
+  // URI - /helloworld
+  // GET
+  // @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+  @GetMapping("helloworld1")
+  public String helloWorld() {
+    return "Hello World";
+  }
+  
+  @GetMapping("helloworld-bean")
+  public UserDetails helloWorldBean() {
+    return new UserDetails("Kalyan", "Reddy", "Hyderabad");
+  }
+}
